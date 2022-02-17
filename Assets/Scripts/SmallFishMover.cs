ce2d9f78d3e08a8490e68504f3cc2512875660b4
giangtthe153299 <giangtthe153299@fpt.edu.vn>
2022-02-17 10:09:26 +0700

    smallFishmove

--- Assets/Scripts/SmallFishMover.cs
+++ Assets/Scripts/SmallFishMover.cs
@@ -4,60 +4,98 @@ using UnityEngine;
 
 public class SmallFishMover : MonoBehaviour
 {
+    public bool facingRight=false;
     Timer timer;
     float normalSpeed = 1f;
-    float runSpeed=3f;
+    float runSpeed = 3f;
     Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
         timer = gameObject.AddComponent<Timer>();
-        timer.Duration =5f;
+        timer.Duration = 5f;
         timer.Run();
-         direction = new Vector2(Random.Range(-1,1), Random.Range(-1,1));
+        direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(GetDistanceWithPlayer() < 5){
+
+        if (GetDistanceWithPlayer() < 5)
+        {
             Run();
 
         }
-        else{
+        else
+        {
             MoveSlowly();
         }
+           if (direction.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (direction.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
-    void MoveSlowly(){
-        if(timer.Finished){
-            direction = new Vector2(Random.Range(-1,1), Random.Range(-1,1));
+    void MoveSlowly()
+    {
+        if (timer.Finished)
+        {
+            direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
             timer.Run();
 
         }
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-       
-    
-    
-    	gameObject.transform.position = gameObject.transform.position + new Vector3(direction.x*normalSpeed*Time.deltaTime,direction.y*normalSpeed*Time.deltaTime,0);
+
+        GameObject player = GameObject.FindGameObjectWithTag("AShark");
 
 
 
-    }
-     void Run(){
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        float step =runSpeed* Time.deltaTime;
-    
-    	Vector2 point = new Vector2(player.transform.position.x, player.transform.position.y);
-    	transform.position = Vector2.MoveTowards(transform.position, point,-1* step);
+        gameObject.transform.position = gameObject.transform.position + new Vector3(direction.x * normalSpeed * Time.deltaTime, direction.y * normalSpeed * Time.deltaTime, 0);
+
+
 
 
     }
-     float GetDistanceWithPlayer(){
-          GameObject player = GameObject.FindGameObjectWithTag("Player");
-        return  Vector3.Distance(player.transform.position, gameObject.transform.position);
+
+
+    void Run()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("AShark");
+        float step = runSpeed * Time.deltaTime;
+        direction.x =- player.transform.position.x + transform.transform.position.x;
+        Vector2 point = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, point, -1 * step);
 
 
     }
+    float GetDistanceWithPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("AShark");
+        return Vector3.Distance(player.transform.position, gameObject.transform.position);
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.gameObject.tag == "SharkMounth")
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
