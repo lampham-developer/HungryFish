9029ed9f09cd8bfc7f8deba0f40c6476812fe1a6
giangtthe153299 <giangtthe153299@fpt.edu.vn>
2022-02-24 10:07:55 +0700

    Update skill

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,72 +6,77 @@ using System.Collections.Generic;
 
 public class CharacterController : MonoBehaviour
 {
-	public static CharacterController CharacterSingleton;
-	private bool facingRight = true;
+    public static CharacterController CharacterSingleton;
+    private bool facingRight = true;
+    [SerializeField]
+    GameObject explosion;
+    public float maxMoveSpeed = 5;
+    public float smoothTime = 0.3f;
+    public float minDistance = 0;
+    Vector2 currentVelocity;
+    private float force = 50;
 
-	public float maxMoveSpeed = 5;
-	public float smoothTime = 0.3f;
-	public float minDistance = 0;
-	Vector2 currentVelocity;
-	private float force = 50;
+    private float cooldown = 2;
+    private float currentCooldown = 0;
 
-	private float cooldown = 2;
-	private float currentCooldown = 0;
+    public Rigidbody2D rigidbody;
 
-	public Rigidbody2D rigidbody;
+    private float maxHealth = 100;
+    private float currentHealth = 100;
 
-	private float maxHealth = 100;
-	private float currentHealth = 100;
-
-	public Slider healthSlider;
-	public Slider staminaSlider;
-	public Image healthFill;
-	public Gradient healthGradient;
-	AudioSource audio;
+    public Slider healthSlider;
+    public Slider staminaSlider;
+    public Image healthFill;
+    public Gradient healthGradient;
+    AudioSource audio;
 
 
-	private void Awake()
-	{
-		CharacterSingleton = this;
-		// set default value for slider
-		healthSlider.maxValue = maxHealth;
-		healthSlider.value = currentHealth;
-		healthFill.color = healthGradient.Evaluate(1f);
+    private void Awake()
+    {
+        CharacterSingleton = this;
+        // set default value for slider
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+        healthFill.color = healthGradient.Evaluate(1f);
 
-		staminaSlider.maxValue = cooldown;
-		staminaSlider.value = currentCooldown;
-		audio = GetComponent<AudioSource>();
-		
-	}
+        staminaSlider.maxValue = cooldown;
+        staminaSlider.value = currentCooldown;
+        audio = GetComponent<AudioSource>();
 
-	private void Update()
-	{
-		//Add more stamina each fram
-		if (currentCooldown < cooldown) { 
-			currentCooldown += Time.deltaTime; 
-			updateStaminaBar();
+    }
+
+    private void Update()
+    {
+        //Add more stamina each fram
+        if (currentCooldown < cooldown)
+        {
+            currentCooldown += Time.deltaTime;
+            updateStaminaBar();
+        }
+
+        //Decrease shark's health every frame
+        decreaseHealth(Time.deltaTime);
+
+        //check moving & skill
+        Move();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            useSkill();
+        }
+		if(Input.GetKeyDown	(KeyCode.Space)){
+			useSkill2();
 		}
 
-		//Decrease shark's health every frame
-		decreaseHealth(Time.deltaTime);
-
-		//check moving & skill
-		Move();
-
-		if (Input.GetMouseButtonDown(0))
-		{
-			useSkill();
-		}
-		
-	}
+    }
 
 
-	public void Move()
-	{
-		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		// Offsets the target position so that the object keeps its distance.
-		mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
-		transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
+    public void Move()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Offsets the target position so that the object keeps its distance.
+        mousePosition += ((Vector2)transform.position - mousePosition).normalized * minDistance;
+        transform.position = Vector2.SmoothDamp(transform.position, mousePosition, ref currentVelocity, smoothTime, maxMoveSpeed);
 
         //flip the character
         if (currentVelocity.x > 0 && !facingRight)
@@ -85,77 +90,99 @@ public class CharacterController : MonoBehaviour
     }
 
 
-	private void Flip()
-	{
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
-
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
-
-	private void useSkill()
+    private void Flip()
     {
-		if(currentCooldown >= cooldown)
-        {
-			Vector2 destination;
-			if (facingRight)
-			{
-				destination = new Vector2(transform.position.x + 50, transform.position.y);
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
 
-			}
-			else
-			{
-				destination = new Vector2(transform.position.x - 50, transform.position.y);
-			}
-			transform.position = Vector2.LerpUnclamped(transform.position, destination, 0.1f);
-			currentCooldown = 0;
-		}
-		
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
-	private void updateHealthBar()
+    private void useSkill()
     {
-		healthSlider.value = currentHealth;
-		healthFill.color = healthGradient.Evaluate(healthSlider.normalizedValue);
-	}
-
-	public void playAudio(){
-		audio.Play(); 
-	}
-
-	private void updateStaminaBar()
-	{
-		staminaSlider.value = currentCooldown;
-	}
-
-	public void decreaseHealth(float amount)
-    {
-		if(currentHealth > 0)
+        if (currentCooldown >= cooldown)
         {
-			currentHealth -= amount;
-			updateHealthBar();
-		}
+            Vector2 destination;
+            if (facingRight)
+            {
+                destination = new Vector2(transform.position.x + 50, transform.position.y);
 
-		if(currentHealth <= 0)
-        {
-			GameController.GameControllerSingleton.endGame();
+            }
+            else
+            {
+                destination = new Vector2(transform.position.x - 50, transform.position.y);
+            }
+            transform.position = Vector2.LerpUnclamped(transform.position, destination, 0.1f);
+            currentCooldown = 0;
+        }
 
-		}
     }
 
-	public void increaseHealth(float amount)
-	{
-		currentHealth += amount;
+    private void updateHealthBar()
+    {
+        healthSlider.value = currentHealth;
+        healthFill.color = healthGradient.Evaluate(healthSlider.normalizedValue);
+    }
 
-		if (currentHealth > maxHealth)
-		{
-			currentHealth = maxHealth;
-		}
+    public void playAudio()
+    {
+        audio.Play();
+    }
 
-		updateHealthBar();
+    private void updateStaminaBar()
+    {
+        staminaSlider.value = currentCooldown;
+    }
 
-	}
+    public void decreaseHealth(float amount)
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth -= amount;
+            updateHealthBar();
+        }
+
+        if (currentHealth <= 0)
+        {
+            GameController.GameControllerSingleton.endGame();
+
+        }
+    }
+
+    public void useSkill2()
+    {
+        if (currentCooldown >= cooldown)
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("Creep");
+            foreach (var obj in objs)
+            {
+                if (Vector3.Distance(transform.position, obj.transform.position) < 10)
+                {
+                    Destroy(obj);
+                    increaseHealth(5f);
+                    GameController.GameControllerSingleton.scoreUp(5f);
+                    GameObject explo = Instantiate<GameObject>(explosion, obj.transform.position, Quaternion.identity);
+
+                }
+            }
+            currentCooldown = 0;
+        }
+
+    }
+
+    public void increaseHealth(float amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        updateHealthBar();
+
+    }
 }
